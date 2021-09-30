8fc95a94422aa79e63bd84887a9110e2764659c3
Axl Kennedy <axl.s.kennedy@gmail.com>
2021-09-30 13:56:12 +0300

    spectr

---
+++ js/slide.js
@@ -0,0 +1,9 @@
+$(function () {
+	$("a.scrollto").click(function () {
+		let elementClick = $(this).attr("href")
+		let destination = $(elementClick).offset().top;
+		$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
+		return false;
+	});
+
+});
